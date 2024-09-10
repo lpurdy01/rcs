d6ee3d98574b9a538f432ddd3915c1e54ab5018e
root <root@UbuWork.4qc5uatdf0aurcb3uzvk24ftjd.xx.internal.cloudapp.net>
2024-09-10 04:09:17 +0000

    some install scripts

---
+++ install_openems.sh
@@ -0,0 +1,12 @@
+sudo add-apt-repository universe
+sudo apt update
+sudo apt install -y python3-full
+sudo apt install -y python-is-python3
+sudo apt-get install -y build-essential cmake git libhdf5-dev libvtk7-dev libboost-all-dev libcgal-dev libtinyxml-dev qtbase5-dev libvtk7-qt-dev
+sudo apt-get install -y gengetopt help2man groff pod2pdf bison flex libhpdf-dev libtool
+sudo apt install -y cmake
+pip install numpy matplotlib cython h5py
+cd ~/repos
+git clone --recursive https://github.com/thliebig/openEMS-Project.git
+cd openEMS-Project
+./update_openEMS.sh ~/opt/openEMS --python
